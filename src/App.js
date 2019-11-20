1b8fbc57ba1c0f257c7b7ab85526f5e7efeed639
Isabel Melendez <isa.melendez@ubiquo.io>
2019-11-20 22:15:19 +0000

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import {photos} from './photos'
+
+
+
+class App extends Component  {
+
+  constructor(props){
+    super(props);
+    this.state = {
+    user: {}
+    }
+  }
+
+
+  render() {
+
+    const useStyles = makeStyles(theme => ({
+      root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+      },
+      gridList: {
+        width: 500,
+        height: 450,
+      },
+    }));
+    
+
+    return (
+      <div className="App">
+          <div class="title"> Tu jauria.com </div>
+          <div class="containerDogs">
+            <GridList cellHeight={160}  cols={3}>
+              {photos.map(tile => (
+                <GridListTile key={tile.img} cols={tile.cols || 1}>
+                  <img src={tile.img} />
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
+      </div>
+    )
+  }
 }
 
+
 export default App;
+
+
+
---
+++ src/photos.js
@@ -0,0 +1,197 @@
+export const photos = [
+    {
+      img: require("./images/image_00268.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00154.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00311.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00399.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_03286.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_02485.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_02777.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_02827.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_02636.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_02585.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_02296.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00438.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00544.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00000.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00001.png"),
+      width: 1,
+      height: 1
+    },
+    {
+      img: require("./images/image_00002.png"),
+      width: 3,
+      height: 4
+    },
+    {
+      img: require("./images/image_00003.png"),
+      width: 3,
+      height: 4
+    },
+    {
+      img: require("./images/image_00004.png"),
+      width: 3,
+      height: 4
+    },
+    {
+      img: require("./images/image_00005.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00006.png"),
+      width: 3,
+      height: 4
+    },
+    {
+      img: require("./images/image_00008.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00009.png"),
+      width: 3,
+      height: 4
+    },
+    {
+      img: require("./images/image_00010.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00011.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00012.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00013.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00014.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00015.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00019.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00017.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00034.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00037.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00040.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00043.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00060.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00085.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00102.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00141.png"),
+      width: 4,
+      height: 3
+    },
+    {
+      img: require("./images/image_00201.png"),
+      width: 4,
+      height: 3
+    },
+  ];
